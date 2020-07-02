70394f7add13caf70fb87d77ae9d146f51fb8086
Moose Fawaz <moosefawaz@gmail.com>
2020-07-02 11:27:14 -0400

    Update main.c
    Update comments

--- main.c
+++ main.c
@@ -24,8 +24,6 @@ int main(int argc, char *argv[]) {
   createPageTable();
   createTLB();
   createBackStore();
-  //the following function calls will initialize the arrays that act as the pagetable and tlb to -1 (implying empty)
-  //while initializing the backstore the given address values in the .bin file.
 
 	/* OPEN ADDRESSES.TXT AND OUTPUT FILE*/
   //Input file to be opened with error checking
@@ -43,19 +41,17 @@ int main(int argc, char *argv[]) {
   start = clock(); //Begin clock counter
   
   //Parse address file and output respective page numbers and offsets
-  //Page table additions and hits/faults will occur within this loop
-  while( !feof(addressFile) ) { //loop will end only when EOF is reached in the address file.
+  while( !feof(addressFile) ) { 
 
-
-    fgets(address, 7, addressFile); //will take an address that is 7(including newline) characters long, this will go line by line through the address file and store it in the address variable.
-    sscanf(address, "%d", &currentAddress); //will convert the address variable into an int and store it in currentAddress
+    // retrieve address from file
+    fgets(address, 7, addressFile); 
+    sscanf(address, "%d", &currentAddress);
 
     //Calculate offset and page number of each address
-    offset = currentAddress & 0xff; //&ing the value of the address that we have with 11111111 this will preserve the value of the first 8 bits.
-    pageNumber = currentAddress >> 8; //then we bit shift to the left by 8 bits, and this gives us the the bits that were at position 8-15 which represents the page number.
-
+    offset = currentAddress & 0xff; 
+    pageNumber = currentAddress >> 8; 
+	  
     //Check TLB for pageNumber
-    //initially TLB is checked as it will make the process faster if this entry has already been done before.
     frameNum = checkTLB(pageNumber);
 
     /* With demand paging, the first series of pages will fault because
@@ -65,13 +61,13 @@ int main(int argc, char *argv[]) {
     if(frameNum != -1) { //If it exists in TLB
 
       //Determine physical address
-      physicalAddress = frameNum + offset; //adding offset and framenumber will give us the physical address
-      byteValue = physicalMem[physicalAddress];//we pass the physicalAddress into our physical memory array to obtain the value in that address and store it in 'byteValue'.
+      physicalAddress = frameNum + offset; 
+      byteValue = physicalMem[physicalAddress];
 
     //TLB Miss, check Page Table
     } else {
 
-      //Checking Page table for Frame
+      
       frameNum = checkPageTable(pageNumber);
 
       //No page fault, frame exists
@@ -79,7 +75,7 @@ int main(int argc, char *argv[]) {
 
         //Calculate physical address and add it to the TLB
         physicalAddress = frameNum + offset;
-        addToTLB(frameNum, pageNumber); //frameNum is added to the first column, pageNumber to the second column
+        addToTLB(frameNum, pageNumber); 
         byteValue = physicalMem[physicalAddress];
 
         //PAGE FAULT
@@ -93,13 +89,12 @@ int main(int argc, char *argv[]) {
         if(physMemPointer != -1) {
 
           //Page retrieved from backing_store.bin and into the free frame
-          //memcpy(dest, src, size) - this will copy data from the .bin file into the physicalMem array. the size of the entry will be PAGE_SIZE = 256
           memcpy(physicalMem + physMemPointer, backStoreMap + pageFront, PAGE_SIZE);
 
           //Determine physical address and value output based on backing_store addition
-          frameNum = physMemPointer; //frameNum will now point to its dedicated memory.
-          physicalAddress = frameNum + offset; //the address will be the offset added to the frameNum
-          byteValue = physicalMem[physicalAddress]; //to access the value we just pass the physicalAddress to our physical storage array.
+          frameNum = physMemPointer; 
+          physicalAddress = frameNum + offset; 
+          byteValue = physicalMem[physicalAddress]; 
 
           //Insert frame number into page table and update TLB, now this value will exist.
           pageTable[pageNumber] = physMemPointer;
@@ -126,13 +121,13 @@ int main(int argc, char *argv[]) {
     fprintf(outputFile, "Value: %d\n", byteValue);
     addressCount++; //Count number of addresses manipulated
   }
-
+  
+  // end clock counter & calclulate total time
   finish = clock();
-  // the start and finish numbers are the number of clock ticks elapsed since the program was launched. dividing by CLOCKS_PER_SEC will give us the number of seconds used by the CPU
   loop_time = finish - start;
   printf("loop time = %ld clock ticks\n", loop_time);
   total_time = ((double) (finish - start)) / CLOCKS_PER_SEC;
-  //number of seconds will be 0 on our machine as the program is executed too quickly
+  
 
   /* WRITE TO OUTPUT FILE HERE */
   fprintf(outputFile, "Number of Translated Addresses: %d\n", addressCount);
